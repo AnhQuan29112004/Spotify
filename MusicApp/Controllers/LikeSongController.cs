580c9bae508cc12b35475a9d35f0f32eb8fab341
AnhQuan29112004 <namcaodidong2004@gmail.com>
2024-12-22 15:50:40 +0700

    .

--- MusicApp/Controllers/LikeSongController.cs
+++ MusicApp/Controllers/LikeSongController.cs
@@ -29,7 +29,7 @@ namespace MusicApp.Controllers
                               join song in db.mic_songs on singer.singer_id equals song.singer_id
                               join likesong in db.mic_favourite_songs on song.song_id equals likesong.song_id
                               join users in db.mic_users on likesong.user_id equals users.user_id
-                              where song.is_deleted == '0' && users.user_id == int.Parse(Session["user_id"].ToString())
+                              where likesong.is_deleted == '0' && users.user_id == int.Parse(Session["user_id"].ToString())
                               select new SingerSongViewModel
                               {
                                   SingerUrl = singer.singer_url,
